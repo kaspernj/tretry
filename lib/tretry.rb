411aae5f01d01b1c00bc85c84e353d334a3b80af
kaspernj <k@spernj.org>
2014-11-21 16:56:13 +0100

    Implemented before_retry. Simplified various used arguments. Made it possible to create as an object.

--- lib/tretry.rb
+++ lib/tretry.rb
@@ -1,5 +1,8 @@
 #A library for doing retries in Ruby with timeouts, analysis of errors, waits between tries and more.
 class Tretry
+  attr_reader :fails
+  attr_accessor :timeout, :tries, :wait
+
   #Valid keys that can be given as argument for the method 'try'.
   VALID_KEYS = [:tries, :timeout, :wait, :interrupt, :exit, :errors, :return_error]
 
@@ -12,54 +15,68 @@ class Tretry
   #  puts "Tries: '#{res[:tries]}'."
   #  puts "Result: '#{res[:result}'."
   def self.try(args = {}, &block)
-    Tretry.new(args, &block).try
+    Tretry.new(args).try(&block)
   end
 
-  def initialize(args, &block)
+  def initialize(args = {})
     @args = args
-    @args[:tries] ||= 3
-    @block = block
-    @tries = []
+    @fails = []
+    @before_retry = []
 
-    validate_arguments
+    parse_arguments
   end
 
-  def try
-    @args[:tries].to_i.downto(1) do |count|
-      @error = nil
+  def before_retry(&block)
+    @before_retry << block
+  end
+
+  def try(&block)
+    raise "No block given." unless block
+    @block = block
+
+    @tries.times do |count|
+      @count = count
+
+      unless first_try?
+        # Sleep for a given amount of time if the 'wait'-argument is given.
+        sleep(@wait) if @wait
+
+        call_before_retry(error: @error)
+        @error = nil
+      end
 
       begin
-        if @args[:timeout]
-          try_with_timeout(count)
+        # If a timeout-argument has been given, then run the code through the timeout.
+        if @timeout
+          try_with_timeout
         else
           # Else call block normally.
           @res = @block.call
           @dobreak = true
-          break
         end
       rescue Exception => e
-        handle_error(e, count)
+        handle_error(e)
       end
 
       if @doraise
         if @args[:return_error]
-          @tries << {error: @error}
+          @fails << {error: @error}
           return {
-            tries: @tries,
+            fails: @fails,
             error: true
           }
         else
           raise @error
         end
       elsif @error
-        @tries << {error: @error}
+        @fails << {error: @error}
       end
 
       break if @dobreak
     end
 
     return {
-      tries: @tries,
+      fails: @fails,
       result: @res,
       error: false
     }
@@ -67,46 +84,57 @@ class Tretry
 
 private
 
-  def validate_arguments
+  def parse_arguments
     #Validate given arguments and set various variables.
-    raise "No block was given." unless @block
     raise "Expected argument to be a hash." unless @args.is_a?(Hash)
 
     @args.each do |key, val|
       raise "Invalid key: '#{key}'." unless VALID_KEYS.include?(key)
     end
+
+    @args[:tries] ||= 3
+    @tries = @args[:tries].to_i
+    @wait = @args[:wait] ? @args[:wait] : nil
+    @timeout = @args[:timeout] ? @args[:timeout].to_f : nil
   end
 
-  def try_with_timeout(count)
-    #If a timeout-argument has been given, then run the code through the timeout.
+  def try_with_timeout
     begin
       require "timeout"
-      Timeout.timeout(@args[:timeout]) do
+      Timeout.timeout(@timeout) do
         @res = @block.call
         @dobreak = true
-        break
       end
     rescue Timeout::Error => e
-      @doraise = e if count <= 1
-      @error = e
-      sleep(@args[:wait]) if @args[:wait] && !@doraise
+      handle_error(e)
     end
   end
 
-  def handle_error(e, count)
+  def handle_error(e)
     if e.class == Interrupt
       raise e if !@args.key?(:interrupt) || @args[:interrupt]
     elsif e.class == SystemExit
       raise e if !@args.key?(:exit) || @args[:exit]
-    elsif count <= 1 || (@args.key?(:errors) && @args[:errors].index(e.class) == nil)
+    elsif last_try? || (@args.key?(:errors) && !@args[:errors].include?(e.class))
       @doraise = e
     elsif @args.key?(:errors) && @args[:errors].index(e.class) != nil
       #given error was in the :errors-array - do nothing. Maybe later it should be logged and returned in a stats-hash or something? - knj
     end
 
     @error = e
+  end
 
-    #Sleep for a given amount of time if the 'wait'-argument is given.
-    sleep(@args[:wait]) if @args[:wait] && !@doraise
+  def call_before_retry(args)
+    @before_retry.each do |before_retry_block|
+      before_retry_block.call(args)
+    end
+  end
+
+  def last_try?
+    (@count + 1) >= @tries
+  end
+
+  def first_try?
+    @count == 0
   end
 end
